8839d5dbdfff4470a2b49c4d6b8141d436a5dd90
Vosram <63881577+vosram@users.noreply.github.com>
2026-03-05 13:57:22 -0600

    Implement file server

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	const port = ":8080"
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
 	server := &http.Server{Handler: mux, Addr: port}
 	log.Printf("Serving on port: %s", port)
 	log.Fatal(server.ListenAndServe())
