1967301ad98cc63148d938e528d6fed1f04a78af
Vosram <63881577+vosram@users.noreply.github.com>
2026-03-06 09:54:05 -0600

    Add healthcheack endpoint

--- main.go
+++ main.go
@@ -8,8 +8,15 @@ import (
 func main() {
 	const port = ":8080"
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", handlerReadiness)
 	server := &http.Server{Handler: mux, Addr: port}
 	log.Printf("Serving on port: %s", port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
