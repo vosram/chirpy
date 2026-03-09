49b571bd9ccd0035ab102287489a6e0d3e92c590
Vosram <63881577+vosram@users.noreply.github.com>
2026-03-09 14:56:01 -0500

    Setup sqlc and add CreateUser query

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/vosram/chirpy
 
 go 1.25.6
+
+require (
+	github.com/google/uuid v1.6.0
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.11.2
+)
--- main.go
+++ main.go
@@ -1,19 +1,39 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/vosram/chirpy/internal/database"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db             *database.Queries
 }
 
 func main() {
+	godotenv.Load()
+	dbUrl := os.Getenv("DB_URL")
+	if dbUrl == "" {
+		log.Fatal("DB_URL must be set")
+	}
+
+	dbConn, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		log.Fatalf("couldn't open db: %s\n%v\n", dbUrl, err)
+	}
+	dbQueries := database.New(dbConn)
+
 	const port = ":8080"
 	mux := http.NewServeMux()
-	apiConf := apiConfig{}
+	apiConf := apiConfig{db: dbQueries}
+
 	fsHandler := http.StripPrefix("/app/", apiConf.middlewareMetricsInc(http.FileServer(http.Dir("."))))
 	mux.Handle("/app/", fsHandler)
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
