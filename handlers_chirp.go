b6a3a4147aae23993b1005260884769bede256f1
Vosram <63881577+vosram@users.noreply.github.com>
2026-03-10 13:35:28 -0500

    Implement create chirp endpoint

--- handlers_chirp.go
+++ handlers_chirp.go
@@ -3,6 +3,10 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/vosram/chirpy/internal/database"
 )
 
 func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
@@ -33,4 +37,55 @@ func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
 		CleanedBody: censorString(chirp.Body, badwords),
 	}
 	respondWithJSON(w, 200, respData)
+}
+
+func (cfg *apiConfig) handlerCreateChirp(w http.ResponseWriter, r *http.Request) {
+	type jsonBody struct {
+		Body   string    `json:"body"`
+		UserID uuid.UUID `json:"user_id"`
+	}
+	type jsonResponse struct {
+		ID        uuid.UUID `json:"id"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+		Body      string    `json:"body"`
+		UserID    uuid.UUID `json:"user_id"`
+	}
+	var chirp jsonBody
+	err := json.NewDecoder(r.Body).Decode(&chirp)
+	if err != nil {
+		responseWithError(w, http.StatusInternalServerError, "Couldn't parse json body", err)
+		return
+	}
+
+	if len(chirp.Body) > 140 {
+		responseWithError(w, http.StatusBadRequest, "Chirp is too long", nil)
+		return
+	}
+	// censor chirp then insert to db
+
+	badwords := map[string]struct{}{
+		"kerfuffle": {},
+		"sharbert":  {},
+		"fornax":    {},
+	}
+	cleanBody := censorString(chirp.Body, badwords)
+	newChirp, err := cfg.db.CreateChirp(r.Context(), database.CreateChirpParams{
+		Body:   cleanBody,
+		UserID: chirp.UserID,
+	})
+
+	if err != nil {
+		responseWithError(w, http.StatusInternalServerError, "Couldn't create chirp in database", err)
+		return
+	}
+
+	res := jsonResponse{
+		ID:        newChirp.ID,
+		CreatedAt: newChirp.CreatedAt,
+		UpdatedAt: newChirp.UpdatedAt,
+		Body:      newChirp.Body,
+		UserID:    newChirp.UserID,
+	}
+	respondWithJSON(w, http.StatusCreated, res)
 }
